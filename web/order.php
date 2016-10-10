98b0c6b0290888eca11b695420d4cf8fba005bcb
Koichi Miyamoto <m5813k@gmail.com>
2016-10-11 03:55:50 +0900

    order.php confirm.php

---
+++ web/order.php
@@ -0,0 +1,17 @@
+
+<?php
+session_start();
+require_once(__DIR__ . "/../util/pdo.php");
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>注文完了</title>
+</head>
+<body>
+  
+注文完了しました
+</body>
+</html>
