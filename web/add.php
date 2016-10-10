da7f81de5c7c6fda1f28c2424fbc458618dd33ee
Koichi Miyamoto <m5813k@gmail.com>
2016-10-10 23:41:25 +0900

    要らないファイル消した

--- web/add.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-echo count($_SESSION['cart']), "<br>";
-echo var_dump($_SESSION['cart']), "<br>";
-$_SESSION['cart'][] = ["goodsid"=>"abo01", "quantity"=> 3];
-echo count($_SESSION['cart']), "<br>";
-echo var_dump($_SESSION['cart']), "<br>";
